90242166a9d8c58504c6dd3bfda2ec75a89dce8c
nicholasMeadows <meadowsnick98@gmail.com>
2024-01-06 20:46:32 -0600

    Update hotkey to close electron

--- electron-entry.js
+++ electron-entry.js
@@ -1,7 +1,7 @@
-import {app, BrowserWindow, globalShortcut} from "electron";
+import { app, BrowserWindow, globalShortcut } from "electron";
 import electronSquirrelStartup from "electron-squirrel-startup";
-import path, {dirname} from "path";
-import {fileURLToPath} from "url";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,61 +9,61 @@ const __dirname = dirname(__filename);
 if (electronSquirrelStartup) app.quit();
 
 const isDev = () => {
-    return process.env.APP_DEV != undefined;
+  return process.env.APP_DEV != undefined;
 };
 const createWindow = () => {
-    isDev();
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    });
-    if (!isDev()) {
-        win.removeMenu();
+  isDev();
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  if (!isDev()) {
+    win.removeMenu();
+  }
+
+  win.loadFile(path.resolve(__dirname, "dist", "index.html"));
+  win.webContents.session.webRequest.onBeforeSendHeaders(
+    (details, callback) => {
+      let requestHeadersToReturn;
+      if (details.requestHeaders["Origin"] != undefined) {
+        details.requestHeaders["Origin"] = "*";
+        requestHeadersToReturn = details.requestHeaders;
+      } else {
+        requestHeadersToReturn = { Origin: "*", ...details.requestHeaders };
+      }
+      callback({
+        requestHeaders: requestHeadersToReturn,
+      });
+    }
+  );
+
+  win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    let responseHeaders;
+    if (details.responseHeaders["access-control-allow-origin"] != undefined) {
+      details.responseHeaders["access-control-allow-origin"] = ["*"];
+      responseHeaders = details.responseHeaders;
+    } else {
+      responseHeaders = {
+        "access-control-allow-origin": ["*"],
+        ...details.responseHeaders,
+      };
     }
 
-    win.loadFile(path.resolve(__dirname, "dist", "index.html"));
-    win.webContents.session.webRequest.onBeforeSendHeaders(
-        (details, callback) => {
-            let requestHeadersToReturn;
-            if (details.requestHeaders["Origin"] != undefined) {
-                details.requestHeaders["Origin"] = "*";
-                requestHeadersToReturn = details.requestHeaders;
-            } else {
-                requestHeadersToReturn = {Origin: "*", ...details.requestHeaders};
-            }
-            callback({
-                requestHeaders: requestHeadersToReturn,
-            });
-        }
-    );
-
-    win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-        let responseHeaders;
-        if (details.responseHeaders["access-control-allow-origin"] != undefined) {
-            details.responseHeaders["access-control-allow-origin"] = ["*"];
-            responseHeaders = details.responseHeaders;
-        } else {
-            responseHeaders = {
-                "access-control-allow-origin": ["*"],
-                ...details.responseHeaders,
-            };
-        }
-
-        callback({
-            responseHeaders: responseHeaders,
-        });
+    callback({
+      responseHeaders: responseHeaders,
     });
+  });
 };
 app.whenReady().then(() => {
-    const quitAccelerator = "CommandOrControl+Q";
-    if (!globalShortcut.isRegistered(quitAccelerator)) {
-        globalShortcut.register(quitAccelerator, () => {
-            app.quit();
-        })
-    }
-    createWindow();
+  const quitAccelerator = "CommandOrControl+W";
+  if (!globalShortcut.isRegistered(quitAccelerator)) {
+    globalShortcut.register(quitAccelerator, () => {
+      app.quit();
+    });
+  }
+  createWindow();
 });
-app.on('will-quit', () => {
-    // Unregister all shortcuts.
-    globalShortcut.unregisterAll()
-})
+app.on("will-quit", () => {
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll();
+});
