fbfbb1301ebd593482062e9b7cdc8b56c8af0aca
nicholasMeadows <meadowsnick98@gmail.com>
2024-11-08 23:21:05 -0600

    Test

--- electron/main.ts
+++ electron/main.ts
@@ -239,7 +239,9 @@ const saveSubredditLists = async (event: IpcMainInvokeEvent, encodedContent: str
   fs.writeFileSync(subredditListsFilePath, encodedContent);
 };
 
+autoUpdater.autoInstallOnAppQuit = false;
+autoUpdater.autoRunAppAfterInstall = true;
 autoUpdater.checkForUpdatesAndNotify({
   title:"Update available",
   body:"There is a new update available"
-});
+})
